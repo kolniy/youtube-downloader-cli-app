aa84e362b6b1a1c28a044ccd951102ba62d4884d
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-08 14:38:05 +0100

    added git ignore files to ignore node modules folder

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import readline from "readline";
+import downloader from "./utitlities/downloader.js";
+
+// const readlineInput = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// readlineInput.question(`What's your name?`, (name) => {
+//   console.log(`Hi ${name}!`);
+//   readlineInput.close();
+// });
+
+downloader();
---
+++ src/utitlities/downloader.js
@@ -0,0 +1,8 @@
+import path from require('path')
+import os from "os"
+import youtubedl from "youtube-dl-exec";
+import logger from "progress-estimator";
+
+const loggerFunc = logger();
+
+// export default downloader;
