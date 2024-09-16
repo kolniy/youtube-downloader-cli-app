96659b9fdd8d94a98edd4a05060f5001b9eee9fb
kolaniyi <kolaniyi3@gmail.com>
2024-09-16 14:03:10 +0100

    added endpoint for geo location

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from "axios";
 import isYoutubeUrl from "./utitlities/validateUrl.js";
 import downloader from "./utitlities/downloader.js";
 
@@ -11,6 +12,10 @@ app.use(express.json({ extended: false }));
 // console.log("Input a url to begin");
 // acceptYoutubeURL();
 
+app.get("/", (req, res) => {
+  res.json({ msg: "welcome to the youtube downloader API" });
+});
+
 app.post("/youtube/download", async (req, res) => {
   const { url } = req.body;
   try {
@@ -22,6 +27,19 @@ app.post("/youtube/download", async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ msg: error.message });
+  }
+});
+
+app.post("/api/v1/google/locator", async (req, res) => {
+  const location = req.body.location;
+  const { lat, lng } = location;
+  try {
+    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=volunteer&location=${lat},${lng}&radius=10000&key=${process.env.googleAPIKEY}&type=volunteer`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.json({ msg: "Error getting position" });
+    console.log(error);
   }
 });
 
